c4f811cf019f340a745272cf8bbc7bf09775a5cd
Raymond Piller <ray@unt.edu>
2017-06-29 23:25:43 -0500

    Updates

--- renderer.js
+++ renderer.js
@@ -3,23 +3,3 @@
 // All of the Node.js APIs are available in this process.
 
 
-var Gdax = require('gdax');
-
-var pubBTCUSDClient = new Gdax.PublicClient('BTC-USD', 'https://api.gdax.com');
-
-var callback = function(err, response, data) {
-	if (err) {
-		console.log("err: "+ err);
-		console.log(err);
-	}
-
-	if (response) {
-		console.log("response: "+ response);
-		console.log(response);
-	}
-
-	if (data) {
-		console.log("data: "+ data);
-		console.log(data);
-	}
-};
